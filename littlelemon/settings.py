4d326c1c560c25de2ad1a26475c155e48923ea0a
mpesaran <mahsa.pesaran@gmail.com>
2023-08-02 15:27:42 +1000

    authentication for booking API

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'reservation',
     'restaurant',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +136,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DJOSER={"USER_ID_FIELD":"username"}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
