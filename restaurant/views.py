982c9b8306ea5ac2ce4729c9edd11b8ebfd3eb18
mpesaran <mahsa.pesaran@gmail.com>
2023-08-04 11:51:49 +1000

    final additions

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,14 +1,15 @@
 #from django.shortcuts import render
 from rest_framework import generics
+from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from .models import Menu, Booking
 from .serializers import menuSerializer, bookingSerializer
 
-# Create your views here.
-#def index(request):
-    #return render (request, 'index.html',{} )
+#Create your views here.
+def index(request):
+    return render (request, 'index.html',{} )
 
 class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
